9a62f3e0168fb3efeb309bb59ee91ffa7f22a4e6
Md Rannu Ahmed (Riyad) <37493558+rbriyad2@users.noreply.github.com>
2022-08-23 21:32:40 +0600

    Add files via upload
    https://agenc.netlify.app/

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.review-item').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0px',
+      });
+})
